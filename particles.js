c5418e8cb057122aa75c4e1b6721200c3b47a0ac
Obviologist <adamheathimage@gmail.com>
2017-03-11 21:53:39 -0700

    Added a seeded RNG, and multiple particle system.

--- particles.js
+++ particles.js
@@ -9,16 +9,41 @@ var tau = Math.PI*2;
 var draw = function () {
     context.save();
     context.translate(halfWidth, halfHeight);
-    circle(0, 0, 50);
+    particleList.forEach(particleLoop);
     context.restore();
+};
 
-}
 
-var circle = function(x, y, radius){
+var particleLoop = function (particle){
+    circle(
+        particle.position.x,
+        particle.position.y,
+        5,
+        particle.hue
+    );
+};
+
+var circle = function(x, y, radius, hue){
     context.beginPath();
     context.ellipse(x, y, radius, radius, 0, 0, tau, false);
     context.closePath();
+    context.fillStyle = 'hsl(' + (hue * 360) + ', 100%, 50%)';
     context.fill();
 };
+
+var Particle = function(seed){
+    this.rng = new RNG(seed);
+    this.position = {
+        x: this.rng.nextRange(-halfWidth, halfWidth),
+        y: this.rng.nextRange(-halfWidth, halfWidth)
+    };
+    this.hue =this.rng.nextFloat();
+};
+
+var particleList = [
+    new Particle(1),
+    new Particle(2),
+    new Particle(3)
+];
 
 draw();
