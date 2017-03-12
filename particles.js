2bc0b208ffff95b3d50be96535d5baa8f9d18ae9
Obviologist <adamheathimage@gmail.com>
2017-03-11 21:06:50 -0700

    Added draw function, and an ellipse function

--- particles.js
+++ particles.js
@@ -1,3 +1,24 @@
-/**
- * Created by Obviologist on 3/11/2017.
- */
+var canvas = document.getElementById('particleCanvas');
+var context = canvas.getContext('2d');
+var width =  canvas.width;
+var height = canvas.height
+var halfWidth = width / 2;
+var halfHeight = height / 2;
+var tau = Math.PI*2;
+
+var draw = function () {
+    context.save();
+    context.translate(halfWidth, halfHeight);
+    circle(0, 0, 50);
+    context.restore();
+
+}
+
+var circle = function(x, y, radius){
+    context.beginPath();
+    context.ellipse(x, y, radius, radius, 0, 0, tau, false);
+    context.closePath();
+    context.fill();
+};
+
+draw();
